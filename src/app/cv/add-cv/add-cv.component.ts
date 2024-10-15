376412e4c1f1b05f8389078283c574e337fb2dd9
aymensellaouti <aymen.sellaouti@gmail.com>
2024-10-15 12:23:45 +0100

    update addCvComponent

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,36 +1,79 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { CvService } from '../services/cv.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { APP_ROUTES } from 'src/config/routes.config';
-import { Cv } from '../model/cv';
+import { Component } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
+import { CvService } from "../services/cv.service";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { APP_ROUTES } from "src/config/routes.config";
+import { Cv } from "../model/cv";
 
 @Component({
-  selector: 'app-add-cv',
-  templateUrl: './add-cv.component.html',
-  styleUrls: ['./add-cv.component.css'],
+  selector: "app-add-cv",
+  templateUrl: "./add-cv.component.html",
+  styleUrls: ["./add-cv.component.css"],
 })
 export class AddCvComponent {
   constructor(
     private cvService: CvService,
     private router: Router,
-    private toaster: ToastrService
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder
   ) {}
 
-  addCv(cv: Cv) {
-    this.cvService.addCv(cv).subscribe({
-      next: () => {
-        this.toaster
-          .success(`Le cv a été ajouté avec succès`);
+  form = this.formBuilder.group(
+    {
+      name: ["", Validators.required],
+      firstname: ["", Validators.required],
+      path: [""],
+      job: ["", Validators.required],
+      cin: [
+        "",
+        {
+          validators: [Validators.required, Validators.pattern("[0-9]{8}")],
+        },
+      ],
+      age: [
+        0,
+        {
+          validators: [Validators.required],
+        },
+      ],
+    },
+  );
+
+  addCv() {
+    this.cvService.addCv(this.form.value as Cv).subscribe({
+      next: (cv) => {
         this.router.navigate([APP_ROUTES.cv]);
+        this.toastr.success(`Le cv ${cv.firstname} ${cv.name}`);
       },
-      error: (erreur) => {
-        console.log(erreur);
-        this.toaster.error(
-          `Problème avec le serveur veuillez contacter l'admin`
+      error: (err) => {
+        this.toastr.error(
+          `Une erreur s'est produite, Veuillez contacter l'admin`
         );
       },
     });
+  }
+
+  get name(): AbstractControl {
+    return this.form.get("name")!;
+  }
+  get firstname() {
+    return this.form.get("firstname");
+  }
+  get age(): AbstractControl {
+    return this.form.get("age")!;
+  }
+  get job() {
+    return this.form.get("job");
+  }
+  get path() {
+    return this.form.get("path");
+  }
+  get cin(): AbstractControl {
+    return this.form.get("cin")!;
   }
 }
