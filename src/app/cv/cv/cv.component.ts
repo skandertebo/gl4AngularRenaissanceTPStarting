43c7f7151f77e5a4789e626f6cbc1b4b132c618b
ykharrat <yasmine.kharrat@covectors.io>
2024-11-27 20:49:13 +0100

    tweaks to junior/senior

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -10,30 +10,37 @@ import { catchError, map, Observable, of } from "rxjs";
   styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs$: Observable<Cv[]> = this.cvService.getCvs().pipe(
-    catchError(() => {
-      this.toastr.error(`
-        Attention!! Les données sont fictives, problème avec le serveur.
-        Veuillez contacter l'admin.`);
-      return of(this.cvService.getFakeCvs());
-    })
-  );
-
-  juniors$: Observable<Cv[]> = this.cvs$.pipe(
-    map((cvs) => cvs.filter((cv) => cv.age < 40))
-  );
-
-  seniors$: Observable<Cv[]> = this.cvs$.pipe(
-    map((cvs) => cvs.filter((cv) => cv.age >= 40))
-  );
+  cvs$: Observable<Cv[]> ;
+  juniors$: Observable<Cv[]> ; 
+  seniors$: Observable<Cv[]> ;
+  selectedCv: Cv | null = null;
+  currentTab: "junior" | "senior" = "junior";
   date = new Date();
   constructor(
     private logger: LoggerService,
     private toastr: ToastrService,
     private cvService: CvService
   ) {
+    this.cvs$=this.cvService.getCvs().pipe(
+      catchError( error => {
+        this.toastr.error(`
+          Attention!! Les données sont fictives, problème avec le serveur.
+          Veuillez contacter l'admin.`);
+        return of(this.cvService.getFakeCvs());
+        
+      }),
+    );
+    this.juniors$=this.cvs$.pipe(
+      map(cvs=>cvs.filter(cv=>cv.age<40))
+    );
+    this.seniors$=this.cvs$.pipe(
+      map(cvs=>cvs.filter(cv=>cv.age>=40))
+    );
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
+  }
+  changeTab(tab: 'junior' | 'senior') {
+    this.currentTab = tab;
   }
   selectedCv$: Observable<Cv> = this.cvService.selectCv$;
 }
