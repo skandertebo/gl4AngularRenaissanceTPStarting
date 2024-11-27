254d688770d4429eb8a716cc05aa153a1218ee53
ykharrat <yasmine.kharrat@covectors.io>
2024-11-27 22:12:11 +0100

    add products route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { AddCvComponent } from "./cv/add-cv/add-cv.component";
 import { CvComponent } from "./cv/cv/cv.component";
 import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
 import { RhComponent } from "./optimizationPattern/rh/rh.component";
+import { ProductsComponent } from "./products/products.component";
 
 const routes: Route[] = [
   { path: "login", component: LoginComponent },
@@ -22,6 +23,7 @@ const routes: Route[] = [
   },
   { path: "cv/add", component: AddCvComponent, canActivate: [AuthGuard] },
   { path: "cv/:id", component: DetailsCvComponent },
+  { path: "products", component: ProductsComponent },
   {
     path: "",
     component: FrontComponent,
