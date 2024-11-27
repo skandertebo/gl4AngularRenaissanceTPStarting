831a35593ad5cf06289271ca8ee87a2a84d231b3
ykharrat <yasmine.kharrat@covectors.io>
2024-11-27 20:32:20 +0100

    add juniors/seniors tab

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -3,28 +3,37 @@ import { Cv } from "../model/cv";
 import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
-import { catchError, Observable, of } from "rxjs";
+import { catchError, map, Observable, of } from "rxjs";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
   styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs$: Observable<Cv[]> = this.cvService.getCvs();
+  cvs$: Observable<Cv[]> = this.cvService.getCvs().pipe(
+    catchError(() => {
+      this.toastr.error(`
+        Attention!! Les données sont fictives, problème avec le serveur.
+        Veuillez contacter l'admin.`);
+      return of(this.cvService.getFakeCvs());
+    })
+  );
+
+  juniors$: Observable<Cv[]> = this.cvs$.pipe(
+    map((cvs) => cvs.filter((cv) => cv.age < 40))
+  );
+
+  seniors$: Observable<Cv[]> = this.cvs$.pipe(
+    map((cvs) => cvs.filter((cv) => cv.age >= 40))
+  );
   date = new Date();
   constructor(
     private logger: LoggerService,
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvs$=this.cvService.getCvs().pipe(catchError(()=>{
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-          return of(this.cvService.getFakeCvs());
-    }));
-      this.logger.logger("je suis le cvComponent");
-      this.toastr.info("Bienvenu dans notre CvTech");
-    }
-    selectedCv$: Observable<Cv> = this.cvService.selectCv$;
+    this.logger.logger("je suis le cvComponent");
+    this.toastr.info("Bienvenu dans notre CvTech");
+  }
+  selectedCv$: Observable<Cv> = this.cvService.selectCv$;
 }
